3fb60c092cef97fe58e7e96021cbec98cce2033d
roger <roger.herikstad@gmail.com>
2017-05-03 18:48:22 +0800

    Initial commit

---
+++ src/EyelinkLive.jl
@@ -0,0 +1,26 @@
+module EyelinkLive
+using Eyelink
+using StaticArrays
+#TODO: Make this more general
+_path = "/Library/Frameworks/eyelink_core.framework/Versions/Current/"
+if !(_path in Libdl.DL_LOAD_PATH)
+  push!(Libdl.DL_LOAD_PATH, "/Library/Frameworks/eyelink_core.framework/Versions/Current/")
+end
+
+
+"""
+Open a connection to eyelink. `Mode` decides the type of connection 
+"""
+open_connection(mode::Int16) = ccall((:open_eyelink_connection, "eyelink_core"), Int16, (Int16,), mode)
+
+open_broadcast_connection = ccall((:eyelink_broadcast_open, "eyelink_core"), Int16, ())
+
+function get_newest_sample()
+  sample = Eyelink.FSAMPLE()
+  ret = ccall((:eyelink_newest_float_sample, "eyelink_core"), Int16, (Ptr{Void},),C_NULL)
+  if ret > 0
+    ret = ccall((:eyelink_newest_float_sample, "eyelink_core"), Int16, (Ptr{Eyelink.FSAMPLE},),sample)
+  end
+  sample
+end
+end  # module
