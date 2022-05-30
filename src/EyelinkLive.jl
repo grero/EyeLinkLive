37a9cecdb53c6f3df7b465527eedcad9009b22a4
roger <roger.herikstad@gmail.com>
2022-05-30 11:50:29 +0800

    Adds some test functions

--- src/EyelinkLive.jl
+++ src/EyelinkLive.jl
@@ -9,7 +9,7 @@ end
 
 
 """
-Open a connection to eyelink. `Mode` decides the type of connection 
+Open a connection to eyelink. `Mode` decides the type of connection
 """
 open_connection(mode::Int16) = ccall((:open_eyelink_connection, "eyelink_core"), Int16, (Int16,), mode)
 
@@ -36,4 +36,58 @@ function get_newest_sample!(sample::Eyelink.FSAMPLE)
   end
   sample
 end
+
+"""
+Continuously collect saccade velocity by listening for `ENDSACC` events
+"""
+function get_saccade_velocity(pvel::Observable{Float64})
+    ev = Ref{FEVENT}(zero(FEVENT))
+    saccades = Matrix{Float64}[]
+    while true
+        ii = ccall((:eyelink_get_next_data, "eyelink_core"), Int16, (Ptr{Void},), C_NULL)
+        if ii == datatypes[:endsacc]
+            ccall((:eyelink_get_data_data, "eyelink_core"), Int16, (Ref{FEVENT},), ev)
+            pvel[] = ev.pvel
+        end
+    end
+end
+
+"""
+Listen for message events from eyelink
+"""
+function get_message_event(msg::Observable{Tuple{UInt32, String}})
+    ev = Ref{FEVENT}(zero(FEVENT))
+    while true
+        ii = ccall((:eyelink_get_next_data, "eyelink_core"), Int16, (Ptr{Void},), C_NULL)
+        if ii == datatypes[:messageevent]
+            ccall((:eyelink_get_data_data, "eyelink_core"), Int16, (Ref{FEVENT},), ev)
+            msg[] = (ev.time, unsafe_string(convert(Ptr{UInt8}, ev.message + sizeof(UInt16)), ev.message.length))
+        end
+    end
+end
+
+"""
+Get the first saccade after the response cue
+"""
+function get_response_saccade(pvel::Observable{Float64}, trigger::String="00000101")
+    response_on = false
+    saccade_seen = false
+    while true
+        ii = ccall((:eyelink_get_next_data, "eyelink_core"), Int16, (Ptr{Void},), C_NULL)
+        if ii == datatypes[:messageevent] & !response_on
+            msg = unsafe_string(convert(Ptr{UInt8}, ev.message + sizeof(UInt16)), ev.message.length)
+            msg = replace(msg, " " => "")
+            if msg == trigger
+                response_on = true
+                saccade_seen = false
+            end
+        elseif ii == datatypes[:endsacc] & response_on & !saccade_seen
+            ccall((:eyelink_get_data_data, "eyelink_core"), Int16, (Ref{FEVENT},), ev)
+            saccade_seen = true
+            response_on = false
+            pvel[] = ev.pvel
+        end
+    end
+end
+
 end  # module
